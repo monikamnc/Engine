3e64adc6d46fd4148951c3beea10dea5112d0f22
Monica <monicamartin13@hotmail.com>
2021-11-19 20:57:34 +0100

    Main Menu Bar

--- Source/ModuleEditor.cpp
+++ Source/ModuleEditor.cpp
@@ -63,7 +63,25 @@ update_status ModuleEditor::Update()
 	ImGui::NewFrame();
 
 	//ImGui::ShowDemoWindow(&App->window->show_another_window); //Demo Window
-
+	if (ImGui::BeginMainMenuBar())
+	{
+		if (ImGui::BeginMenu("File"))
+		{
+			//ShowExampleMenuFile();
+			ImGui::EndMenu();
+		}
+		if (ImGui::BeginMenu("Edit"))
+		{
+			if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+			if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+			ImGui::Separator();
+			if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+			if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+			if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+			ImGui::EndMenu();
+		}
+		ImGui::EndMainMenuBar();
+	}
 
 	ImGui::Begin("Console");                          // Console Window
 
