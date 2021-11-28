b77395b33dcab56b983da2fbe7e0f2fb9e88d092
Monica <monicamartin13@hotmail.com>
2021-11-28 02:58:03 +0100

    Mesh Class started

--- Source/Mesh.h
+++ Source/Mesh.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <assimp/scene.h>
+#include <assimp/material.h>
+#include <assimp/mesh.h>
+#include "MathGeoLib.h"
+#include "GL/glew.h"
+
+
 class Mesh
 {
+public:
+	Mesh();
+	~Mesh();
+
+	void LoadVBO(const aiMesh* mesh);
+	void LoadEBO(const aiMesh* mesh);
+	void CreateVAO();
+	void Draw(const std::vector<unsigned>& model_textures);
+
+private:
+
+	unsigned vbo,ebo,vao;
+	unsigned num_vertices, num_indices, material_index;
 };
 
